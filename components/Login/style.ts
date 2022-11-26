b795dea5c836a224861661c1fdd6ccf6ec23e03b
iamwebluk <lukas.weber@edu.fh-joanneum.at>
2022-11-26 17:24:03 +0100

    added and cleaned code

--- components/Login/style.ts
+++ components/Login/style.ts
@@ -1,34 +1,16 @@
 import { StyleSheet } from 'react-native';
 const style = StyleSheet.create({
     container: {
-        flex: 1,
-        marginTop: '50%',
+        marginTop: '40%',
     },
-    email: {
-        borderColor: 'grey',
-        borderRadius: 1,
-        borderWidth: 1,
+    textField: {
         padding: 10,
-        height: 60,
-    },
-    password: {
-        borderColor: 'grey',
-        borderWidth: 1,
-        padding: 10,
-        flexDirection: 'row',
-        alignItems: 'baseline',
-        alignSelf: 'stretch',
-        height: 60,
     },
     inputBorder: {
-        // border: '1px solid black',
-        // border
-        // border: '1px solid black',
         borderWidth: 1,
         borderColor: 'red',
     },
     buttonStyle : {
-      // backgroundColor: 'green',
       color: 'white',
       marginTop: 50,
     },
---
+++ components/Register/style.ts
@@ -0,0 +1,21 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+
+const style = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        marginTop: '10%',
+    },
+    password: {
+        borderColor: 'grey',
+        borderWidth: 1,
+        flexDirection: 'row',
+        alignItems: 'baseline',
+        alignSelf: 'stretch',
+        height: 60,
+        width: '100%',
+    },
+})
+
+export default style
