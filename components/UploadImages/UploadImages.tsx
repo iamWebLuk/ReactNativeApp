51c949d7cf1ef122c07c73a63d4dc04b9986d6c7
iamwebluk <lukas.weber@edu.fh-joanneum.at>
2022-12-04 21:57:09 +0100

    added camera

--- components/UploadImages/UploadImages.tsx
+++ components/UploadImages/UploadImages.tsx
@@ -1,13 +1,55 @@
-import { getStorage, ref } from 'firebase/storage';
-import { storage } from '../../firebase';
-import { Text } from 'react-native';
-
+import { Button, Text, TouchableOpacity, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { textColor } from '../../globalStyles';
 const UploadImages = () => {
     // const storageRef = ref(storage);
     //
     // const imageRef = ref(storageRef, 'images');
+    const [type, setType] = useState(CameraType.back);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
 
-    return <Text>Hallo</Text>;
+    const toggleCameraType = () => {
+        setType((current) =>
+            current == CameraType.back ? CameraType.front : CameraType.back
+        );
+    };
+
+    console.log(permission?.granted + ' permission');
+
+    const startCamera = async () => {
+        const abc = requestPermission();
+        abc.then(() => {
+            console.log('erlaubt');
+        }).catch((err) => {
+            alert(err);
+        });
+    };
+
+    return (
+        <View style={{ backgroundColor: 'white', height: '100%' }}>
+            <Camera type={type} style={{ height: '100%' }}>
+                <View>
+                    <TouchableOpacity onPress={toggleCameraType}>
+                        <Text
+                            style={{
+                                color: textColor(),
+                                backgroundColor: 'red',
+                                position: 'relative',
+                                bottom: 0,
+                            }}
+                        >
+                            Flip Camera
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity onPress={}>
+                    <Text>Picture</Text>
+                </TouchableOpacity>
+            </Camera>
+            <Button title={'hallo'} onPress={() => console.log('abc')}></Button>
+        </View>
+    );
 };
 
 export default UploadImages;
